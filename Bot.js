df82e2db21d33e9a95e4eb0983ad12780fbd650e
CodebenderSu <codebendersu@gmail.com>
2018-12-31 16:03:38 -0700

    [Feature] Added error and debug logging

--- Bot.js
+++ Bot.js
@@ -27,6 +27,11 @@ client.on('message', (message) => {
     commandParser(message, rawCommand);
   };
 });
+//////////////////////* ERROR HANDLERS */////////////////////////
+client.on("error", (e) => console.error('[Error]', e.message));
+client.on("warn", (e) => console.warn('[Warning]', e.message));
+//client.on("debug", (e) => console.info('[Debug]', e.message));
+
 ///////////////////////////* SERVER PURGE *////////////////////////
 //let guildList = client.guilds.map(guild => guild.name).join('\n');
 //console.log(`Servers: \n${guildList}`);
